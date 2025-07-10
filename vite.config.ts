273ff80256be94add48b259bf617df2f3c93c035
TY Mathers <alexmathers19@gmail.com>
2025-07-09 17:19:00 -0700

    removed vercel plugin stuff, it works without

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: process.env.PORT as unknown as number,
   },
-  plugins: [react(), tailwindcss(), vercel()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
